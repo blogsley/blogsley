15dc6045566fe20ab09cd7d4eb624fceb5fe42bb
Kurtis Fields <kurtisfields@gmail.com>
2020-11-26 21:00:18 -0500

    work

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,9 @@
 const { execSync } = require('child_process')
 
-// execSync('rimraf dist', { stdio: 'inherit'})
+execSync('rimraf dist', { stdio: 'inherit'})
 
 const { SITE_ID } = process.env
-
+/*
 switch (SITE_ID) {
   case '@blocksley/blocksley-demo':
     execSync('yarn workspace @blocksley/blocksley-demo deploy', { stdio: 'inherit'})
@@ -12,3 +12,6 @@ switch (SITE_ID) {
     execSync('yarn workspace @blogsley/blogsley deploy', { stdio: 'inherit'})
     break;  
 }
+*/
+
+execSync('yarn workspace @blogsley/blogsley deploy', { stdio: 'inherit'})
